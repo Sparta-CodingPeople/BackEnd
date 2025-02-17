86ac5e128428849b73c8634c2edffbc9c1b2f286
kizzis <iswater.kim@gmail.com>
2025-02-18 00:51:13 +0900

    [feat] Review & ReviewImage 엔티티 생성

---
+++ src/main/java/com/server/delivery/model/user/entity/Review.java
@@ -0,0 +1,58 @@
+package com.server.delivery.model.user.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.server.delivery.common.BaseEntity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "p_review")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@SQLDelete(sql = "UPDATE p_review SET review_Deleted = true WHERE review_id = ?")
+@SQLRestriction("review_Deleted = false")
+public class Review extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "review_id")
+	private String id;
+
+	private String content;
+
+	private Double rating;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Builder.Default
+	@OneToMany(mappedBy = "review", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<ReviewImage> images = new ArrayList<>();
+
+	@Builder.Default
+	@Column(name = "review_Deleted")
+	private Boolean isDeleted = Boolean.FALSE;
+
+	// todo. 가게 id, 주문 id FK
+
+}
