9f117c2d021dd2bd5815a84adf64a8f098cd2f1f
AnJungGeon <gunn0128@gmail.com>
2025-02-18 16:14:56 +0900

    [fix] foodImage 수정 음식 사진 받기 위해 피드백 반영하여 수정했습니다.

--- src/main/java/com/server/delivery/domain/menu/dto/request/MenuRequestDto.java
+++ src/main/java/com/server/delivery/domain/menu/dto/request/MenuRequestDto.java
@@ -1,15 +1,13 @@
 package com.server.delivery.domain.menu.dto.request;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@Setter
 public class MenuRequestDto {
     private String foodName;
     private String description;
     private int price;
     private boolean availability;
-    private MultipartFile foodImage;
+
 }
