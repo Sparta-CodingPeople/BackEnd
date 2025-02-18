933bc684e9710fb35f6f428fc56636cee8471d39
kizzis <iswater.kim@gmail.com>
2025-02-18 20:50:39 +0900

    [feat] 배송 기능 초기 구조 추가

---
+++ src/main/java/com/server/delivery/domain/delivery/controller/DeliveryController.java
@@ -0,0 +1,45 @@
+package com.server.delivery.domain.delivery.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.server.delivery.common.jwt.CustomUserDetail;
+import com.server.delivery.domain.delivery.dto.req.DeliveryUpdateRequestDto;
+import com.server.delivery.domain.delivery.dto.res.DeliverySearchResponseDto;
+import com.server.delivery.domain.delivery.dto.res.DeliveryUpdateResponseDto;
+import com.server.delivery.domain.delivery.service.DeliveryService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class DeliveryController {
+	private final DeliveryService deliveryService;
+
+	// 배달 정보 조회
+	@GetMapping("/v1/deliveries/{deliveryId}")
+	public ResponseEntity<DeliverySearchResponseDto> searchDeliveryInfo(
+		@PathVariable String deliveryId,
+		@AuthenticationPrincipal CustomUserDetail userDetail
+	) {
+		DeliverySearchResponseDto response = deliveryService.searchDeliveryInfo(userDetail, deliveryId);
+		return ResponseEntity.ok(response);
+	}
+
+	// 배달 상태 변경 (관리자, 사장)
+	@PatchMapping("/v1/deliveries/{deliveryId}/status")
+	public ResponseEntity<DeliveryUpdateResponseDto> updateDeliveryStatus(
+		@PathVariable String deliveryId,
+		@AuthenticationPrincipal CustomUserDetail userDetail,
+		DeliveryUpdateRequestDto request
+	) {
+		DeliveryUpdateResponseDto response = deliveryService.updateDeliveryStatus(deliveryId, userDetail, request);
+		return ResponseEntity.ok(response);
+	}
+}
