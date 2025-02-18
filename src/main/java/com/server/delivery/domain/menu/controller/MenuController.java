05b6ace94dddb85ebaa8f59f02e070a034d52131
AnJungGeon <gunn0128@gmail.com>
2025-02-19 01:14:54 +0900

    [fix] 메뉴 컨트롤러 수정 - 메뉴 컨트롤러 response 값 공통 response로 변경했습니다.

--- src/main/java/com/server/delivery/domain/menu/controller/MenuController.java
+++ src/main/java/com/server/delivery/domain/menu/controller/MenuController.java
@@ -1,8 +1,10 @@
 package com.server.delivery.domain.menu.controller;
 
 import com.server.delivery.common.PageCustom;
+import com.server.delivery.common.response.CustomResponse;
+import com.server.delivery.domain.menu.dto.request.MenuUpdateRequestDto;
 import com.server.delivery.domain.menu.service.MenuService;
-import com.server.delivery.domain.menu.dto.request.MenuRequestDto;
+import com.server.delivery.domain.menu.dto.request.MenuCreateRequestDto;
 import com.server.delivery.domain.menu.dto.response.MenuResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -17,84 +19,63 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/menus/v1")
+@RequestMapping("/api/v1/menus")
 public class MenuController {
 
     private final MenuService menuService;
 
     // 메뉴 등록
     @PostMapping("/{storeId}")
-    public ResponseEntity<MenuResponseDto> registerMenu(
+    public ResponseEntity<CustomResponse<Void>> registerMenu(
             @PathVariable UUID storeId,
-            @RequestPart("menuData") MenuRequestDto requestDto,
+            @RequestPart("menuData") MenuCreateRequestDto requestDto,
             @RequestPart(value = "foodImage", required = false) MultipartFile foodImage ) {
 
         menuService.registerMenu(storeId, requestDto, foodImage);
 
-        MenuResponseDto response = MenuResponseDto.builder()
-                .status("200")
-                .message("메뉴 등록 성공")
-                .data(null)
-                .build();
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(CustomResponse.success("메뉴 등록 성공"));
     }
 
     // 메뉴 수정
     @PutMapping("/{storeId}/store/{menuId}/menu")
-    public ResponseEntity<MenuResponseDto> updateMenu(
+    public ResponseEntity<CustomResponse<Void>> updateMenu(
             @PathVariable UUID storeId,
             @PathVariable UUID menuId,
-            @RequestBody MenuRequestDto requestDto) {
+            @RequestBody MenuUpdateRequestDto requestDto) {
 
         menuService.updateMenu(storeId, menuId, requestDto);
-
-        MenuResponseDto response = MenuResponseDto.builder()
-                .status("200")
-                .message("메뉴 수정 성공")
-                .data(null)
-                .build();
-
-        return ResponseEntity.ok(response);
+        
+        return ResponseEntity.ok(CustomResponse.success("메뉴 수정 성공"));
     }
 
     // 메뉴 삭제
     @DeleteMapping("/{storeId}/store/{menuId}/menu")
-    public ResponseEntity<MenuResponseDto> deleteMenu(
+    public ResponseEntity<CustomResponse<Void>> deleteMenu(
             @PathVariable UUID storeId,
             @PathVariable UUID menuId) {
 
         menuService.deleteMenu(storeId, menuId);
 
-        MenuResponseDto response = MenuResponseDto.builder()
-                .status("200")
-                .message("메뉴 삭제 성공")
-                .data(null)
-                .build();
+        menuService.deleteMenu(storeId, menuId);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(CustomResponse.success("메뉴 삭제 성공"));
     }
 
     // 메뉴 단일 조회
     @GetMapping("/{storeId}/store/{menuId}/menu")
-    public ResponseEntity<MenuResponseDto> getMenu(
-            @PathVariable UUID restaurantId,
+    public ResponseEntity<CustomResponse<MenuResponseDto>> getMenu(
+            @PathVariable UUID storeId,
             @PathVariable UUID menuId) {
 
-        MenuResponseDto response = menuService.getMenu(restaurantId, menuId);
+        MenuResponseDto response = menuService.getMenu(storeId, menuId);
 
-        MenuResponseDto finalResponse = MenuResponseDto.builder()
-                .status("200")
-                .message("메뉴 조회 성공")
-                .data(response)
-                .build();
-
-        return ResponseEntity.ok(finalResponse);
+        return ResponseEntity.ok(CustomResponse.success("메뉴 조회 성공", response));
     }
 
     // 메뉴 검색
     @GetMapping("/{storeId}/store")
-    public ResponseEntity<PageCustom<MenuResponseDto>> searchMenus(
+    public ResponseEntity<CustomResponse<PageCustom<MenuResponseDto>>> searchMenus(
             @PathVariable UUID storeId,
             @RequestParam("search") String search,
             @RequestParam("size") int size,
@@ -108,6 +89,7 @@ public class MenuController {
 
         PageCustom<MenuResponseDto> pageResultList = new PageCustom<>(resultList, pageable, totalMenus);
 
-        return ResponseEntity.ok(pageResultList);
+        return ResponseEntity.ok(CustomResponse.success("메뉴 검색 결과", pageResultList));
     }
+
 }
