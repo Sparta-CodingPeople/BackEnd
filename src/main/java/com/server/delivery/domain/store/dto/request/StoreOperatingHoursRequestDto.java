2162bee7547296ad2011fde1634420e4f7a729f8
AnJungGeon <gunn0128@gmail.com>
2025-02-19 01:21:29 +0900

    [fix] 매장 운영 시간 requestDto 필드 수정

--- src/main/java/com/server/delivery/domain/store/dto/request/StoreOperatingHoursRequestDto.java
+++ src/main/java/com/server/delivery/domain/store/dto/request/StoreOperatingHoursRequestDto.java
@@ -3,11 +3,13 @@ package com.server.delivery.domain.store.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class StoreOperatingHoursRequestDto {
-    private String openingTime;
-    private String closingTime;
-    private boolean isClosed;
-    private int weekday;
+    private String operationTimeOpeningTime;
+    private String operationTimeClosingTime;
+    private boolean storeIsClosed;
+    private List<String> weekdays;
 }
