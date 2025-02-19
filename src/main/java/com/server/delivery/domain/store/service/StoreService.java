9cc72725a8b7c3c8724cb7eb9de8d94a1516bc33
AnJungGeon <gunn0128@gmail.com>
2025-02-19 20:11:48 +0900

    [feature] 매장 서비스 인터페이스 생성

--- src/main/java/com/server/delivery/domain/store/service/StoreService.java
+++ src/main/java/com/server/delivery/domain/store/service/StoreService.java
@@ -1,59 +1,7 @@
 package com.server.delivery.domain.store.service;
 
-import com.server.delivery.domain.store.dto.request.StoreLocationRequestDto;
-import com.server.delivery.domain.store.dto.request.StoreOperatingHoursRequestDto;
 import com.server.delivery.domain.store.dto.request.StoreRegisterRequestDto;
-import com.server.delivery.domain.store.dto.request.StoreUpdateRequestDto;
-import com.server.delivery.domain.store.dto.response.StoreResponseDto;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
-
-@Service
-public class StoreService {
-
-    public int deleteStore(
-            UUID id,
-            String password) {
-        return 0;
-    }
-
-    public int updateStore(
-            UUID id,
-            StoreUpdateRequestDto requestDto) {
-        return 0;
-    }
-
-    public StoreResponseDto getStore(UUID id) {
-
-        return null;
-    }
-
-    public int updateStoreLocation(
-            UUID id,
-            StoreLocationRequestDto requestDto) {
-        return 0;
-    }
-
-    public int updateStoreOperatingHours(
-            UUID id,
-            List<StoreOperatingHoursRequestDto> requestDto) {
-        return 0;
-    }
-
-    public List<StoreResponseDto> searchStores(
-            String search,
-            Pageable pageable) {
-        return null;
-    }
-
-    public Long getTotalStores(String search) {
-        return null;
-    }
-
-    public int registerStore(StoreRegisterRequestDto requestDto) {
-        return 0;
-    }
+public interface StoreService {
+    void registerStore(StoreRegisterRequestDto requestDto);
 }
