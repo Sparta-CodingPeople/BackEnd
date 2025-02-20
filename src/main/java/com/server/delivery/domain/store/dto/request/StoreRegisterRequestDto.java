001d3266e69f84cc54dda5b54eaf46c9527ed0bb
AnJungGeon <gunn0128@gmail.com>
2025-02-21 03:50:19 +0900

    [fix] 매장 등록 requestDto 수정

--- src/main/java/com/server/delivery/domain/store/dto/request/StoreRegisterRequestDto.java
+++ src/main/java/com/server/delivery/domain/store/dto/request/StoreRegisterRequestDto.java
@@ -3,10 +3,12 @@ package com.server.delivery.domain.store.dto.request;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 public class StoreRegisterRequestDto {
     private StoreInfoDto storeInfo;
     private StoreLocationRequestDto storeLocation;
     private List<StoreOperatingHoursRequestDto> operatingHours;
+    private UUID storeLocationUuid;
 }
