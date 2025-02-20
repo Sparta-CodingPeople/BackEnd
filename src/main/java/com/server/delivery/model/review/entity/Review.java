3b1fdefd3de0a73134902dad6e202764503e996e
kizzis <iswater.kim@gmail.com>
2025-02-20 12:23:59 +0900

    [Refactor] BaseEntity, Menu, Order, Review, Store, User 엔티티 클래스 필드 수정
    -BaseEntity:
    응답값으로 생성자, 생성일 등을 보내기 위해 @Getter 추가
    - Menu:
    Review 엔티티 연관 관계 주석 처리 (명확하게 필요한 기능 필요하면 추가)
    - Order:
    soft delete를 위한 isDeleted 필드 추가
    PK 타입 UUID로 변경
    @SQLDelete와 @SQLRestriction 쿼리 수정
    - Review:
    @SQLDelete 쿼리 수정
    soft delete를 실행하기 위해 ReviewImage cascade 옵션 삭제
    Order와 단방향 일대일 관계로 설정
    Menu 연관 관계 설정은 추후 필요 시 추가
    리뷰 수정을 위한 updateReview 메소드 추가
    -Store:
    @Builder 추가
    @SQLRestriction 추가
    - User:
    Review와 User 양방향 일대다 관계 설정

--- src/main/java/com/server/delivery/model/review/entity/Review.java
+++ src/main/java/com/server/delivery/model/review/entity/Review.java
@@ -8,12 +8,10 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 import com.server.delivery.common.BaseEntity;
-import com.server.delivery.model.menu.entity.Menu;
 import com.server.delivery.model.order.entity.Order;
 import com.server.delivery.model.store.entity.Store;
 import com.server.delivery.model.user.entity.User;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,6 +20,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -51,7 +50,7 @@ public class Review extends BaseEntity {
 	private User user;
 
 	@Builder.Default
-	@OneToMany(mappedBy = "review", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(mappedBy = "review")
 	private List<ReviewImage> images = new ArrayList<>();
 
 	@Builder.Default
@@ -62,11 +61,22 @@ public class Review extends BaseEntity {
 	@JoinColumn(name = "store_uuid")
 	private Store store;
 
-	@ManyToOne
-	@JoinColumn(name = "order_id")
+	@OneToOne
+	@JoinColumn(name = "order_uuid", unique = true)
 	private Order order;
 
-	@ManyToOne
-	@JoinColumn(name = "menu_uuid")
-	private Menu menu;
+	// todo. 필요 시 추가
+	//    @ManyToOne
+	//    @JoinColumn(name = "menu_uuid")
+	//    private Menu menu;
+
+	public void updateReview(String content, Double rating) {
+		this.content = content;
+		this.rating = rating;
+	}
+
+	public void updateReviewImages(List<ReviewImage> images) {
+		this.images.clear();
+		this.images.addAll(images);
+	}
 }
