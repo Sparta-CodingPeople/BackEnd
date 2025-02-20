650f3d90d52bd16cc6b3e6cf81d9256385438b19
Sunro1994 <gwangjulsr@gmail.com>
2025-02-20 20:50:07 +0900

    [Refactor] 필독! Review isDeleted 컬럼명 컨벤션 수정, User import 최적화
    1. Review의 스네이크 케이스에서 대문자 발견하여 소문자로 수정

--- src/main/java/com/server/delivery/model/user/entity/User.java
+++ src/main/java/com/server/delivery/model/user/entity/User.java
@@ -1,14 +1,5 @@
 package com.server.delivery.model.user.entity;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
-import org.springframework.security.core.context.SecurityContextHolder;
-
 import com.server.delivery.common.BaseEntity;
 import com.server.delivery.model.manager.entity.Manager;
 import com.server.delivery.model.master.entity.Master;
@@ -16,26 +7,16 @@ import com.server.delivery.model.owner.entity.Owner;
 import com.server.delivery.model.review.entity.Review;
 import com.server.delivery.model.user.entity.constant.UserGender;
 import com.server.delivery.model.user.entity.constant.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.PreRemove;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -48,78 +29,77 @@ import lombok.Setter;
 @Table(name = "p_user")
 public class User extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_id", nullable = false, updatable = false)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false, updatable = false)
+    private Long id;
 
-	@Column(name = "user_username", nullable = false, unique = true)
-	private String username;
+    @Column(name = "user_username", nullable = false, unique = true)
+    private String username;
 
-	@Column(name = "user_password", nullable = false)
-	private String password;
+    @Column(name = "user_password", nullable = false)
+    private String password;
 
-	@Column(name = "user_first_name", nullable = false)
-	private String firstName;
+    @Column(name = "user_first_name", nullable = false)
+    private String firstName;
 
-	@Column(name = "user_last_name", nullable = false)
-	private String lastName;
+    @Column(name = "user_last_name", nullable = false)
+    private String lastName;
 
-	@Column(name = "user_nickname", nullable = false)
-	private String nickname;
+    @Column(name = "user_nickname", nullable = false)
+    private String nickname;
 
-	@Column(name = "user_phone_number", nullable = false)
-	private String phoneNumber;
+    @Column(name = "user_phone_number", nullable = false)
+    private String phoneNumber;
 
-	@Column(name = "user_profile_image")
-	private String profileImage;
+    @Column(name = "user_profile_image")
+    private String profileImage;
 
-	@Builder.Default
-	@Column(name = "user_is_public", nullable = false)
-	private Boolean isPublic = Boolean.FALSE;
+    @Builder.Default
+    @Column(name = "user_is_public", nullable = false)
+    private Boolean isPublic = Boolean.FALSE;
 
-	@Column(name = "user_role", nullable = false)
-	@Enumerated(EnumType.STRING)
-	private UserRole userRole;
+    @Column(name = "user_role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
 
-	@Column(name = "user_gender", nullable = false)
-	@Enumerated(EnumType.STRING)
-	private UserGender gender;
+    @Column(name = "user_gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserGender gender;
 
-	@Column(name = "user_birth_date", nullable = false)
-	private LocalDate birthDate;
+    @Column(name = "user_birth_date", nullable = false)
+    private LocalDate birthDate;
 
-	@Column(name = "user_token_issued_at")
-	private LocalDateTime tokenIssuedAt;
+    @Column(name = "user_token_issued_at")
+    private LocalDateTime tokenIssuedAt;
 
-	@Column(name = "user_is_deleted")
-	@Builder.Default
-	private Boolean isDeleted = Boolean.FALSE;
+    @Column(name = "user_is_deleted")
+    @Builder.Default
+    private Boolean isDeleted = Boolean.FALSE;
 
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "master_id")
-	private Master master;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "master_id")
+    private Master master;
 
-	@OneToMany(mappedBy = "user")
-	private List<Owner> owners;
+    @OneToMany(mappedBy = "user")
+    private List<Owner> owners;
 
-	@OneToMany(mappedBy = "user")
-	private List<UserStore> userStores;
+    @OneToMany(mappedBy = "user")
+    private List<UserStore> userStores;
 
-	@OneToMany(mappedBy = "user")
-	private List<Manager> managers;
+    @OneToMany(mappedBy = "user")
+    private List<Manager> managers;
 
-	@Builder.Default
-	@OneToMany(mappedBy = "user")
-	private List<Review> reviews = new ArrayList<>();
+    @Builder.Default
+    @OneToMany(mappedBy = "user")
+    private List<Review> reviews = new ArrayList<>();
 
-	public void updateTokenIssuedAt() {
-		this.tokenIssuedAt = LocalDateTime.now();
-	}
+    public void updateTokenIssuedAt() {
+        this.tokenIssuedAt = LocalDateTime.now();
+    }
 
-	@PreRemove
-	public void softDelete() {
-		this.setDeletedAt(LocalDateTime.now());
-		this.setDeletedBy(SecurityContextHolder.getContext().getAuthentication().getName());
-	}
+    public void softDelete() {
+        this.setDeletedAt(LocalDateTime.now());
+        this.setDeletedBy(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
 }
