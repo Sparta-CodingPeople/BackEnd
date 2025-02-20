d08722307790a907344b7e9aca32bdd9645d8230
AnJungGeon <gunn0128@gmail.com>
2025-02-21 03:51:33 +0900

    [fix] ResponseDto 매장 검색 결과 추가

--- src/main/java/com/server/delivery/domain/store/dto/response/StoreResponseDto.java
+++ src/main/java/com/server/delivery/domain/store/dto/response/StoreResponseDto.java
@@ -1,9 +1,12 @@
 package com.server.delivery.domain.store.dto.response;
 
+import com.server.delivery.model.store.entity.OperationTimes;
+import com.server.delivery.model.store.entity.Store;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -17,11 +20,26 @@ public class StoreResponseDto {
     private UUID storeUuid;
     private String storeName;
     private int storeCategoryId;
-    private int storeLocationId;
-    private long storeOperationId;
+    private UUID storeLocationId;
+    private String storeDescription;
+    private String address;
+    private String operationTimeOpeningTime;
+    private String operationTimeClosingTime;
     private String phoneNumber;
     private boolean storeIsDeleted;
     private boolean storeIsGranted;
+
+    public static StoreResponseDto fromEntity(Store store) {
+        return StoreResponseDto.builder()
+                .storeUuid(store.getStoreUuid())
+                .storeName(store.getStoreName())
+                .phoneNumber(store.getPhoneNumber())
+                .storeDescription(store.getStoreDescription())
+                .storeIsDeleted(store.isStoreIsDeleted())
+                .storeIsGranted(store.isStoreIsGranted())
+                .address(store.getStoreLocation().getLocation().getAddress())
+                .build();
+    }
 
 
 }
