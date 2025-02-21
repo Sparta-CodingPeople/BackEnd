850d2fab76df6af5c5eb638c72548cee22453331
AnJungGeon <gunn0128@gmail.com>
2025-02-21 15:59:53 +0900

    [feature] 구글 ai api 연동

---
+++ src/main/java/com/server/delivery/domain/ai/controller/AiController.java
@@ -0,0 +1,23 @@
+package com.server.delivery.domain.ai.controller;
+
+import com.server.delivery.domain.ai.service.AiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/ai")
+public class AiController {
+    private final AiService aiService;
+
+
+
+    @GetMapping("/generate")
+    public String generateContent(@RequestParam String content) {
+
+        return aiService.generateContent(content);
+    }
+}
