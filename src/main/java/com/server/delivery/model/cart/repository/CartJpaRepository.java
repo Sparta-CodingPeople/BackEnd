99f96546f40c91dc32608d44b33634081f7eb9f1
Sunro1994 <gwangjulsr@gmail.com>
2025-02-23 00:20:20 +0900

    [Feature] Cart Repository 메서드 추가

--- src/main/java/com/server/delivery/model/cart/repository/CartJpaRepository.java
+++ src/main/java/com/server/delivery/model/cart/repository/CartJpaRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface CartJpaRepository extends JpaRepository<Cart, String> {
     Optional<Cart> findByUser(User user);
+
+    Optional<Cart> findByCartUuid(UUID cartUuid);
 }
