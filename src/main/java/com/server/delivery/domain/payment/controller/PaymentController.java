0c6d7bdca1597e3b10a13bab4bb01be680c560ea
kizzis <iswater.kim@gmail.com>
2025-02-24 17:18:16 +0900

    [Refactor] 결제 Pagination 리팩토링

--- src/main/java/com/server/delivery/domain/payment/controller/PaymentController.java
+++ src/main/java/com/server/delivery/domain/payment/controller/PaymentController.java
@@ -1,21 +1,5 @@
 package com.server.delivery.domain.payment.controller;
 
-import java.util.UUID;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.PagedModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.server.delivery.common.jwt.CustomUserDetail;
 import com.server.delivery.common.response.CustomResponse;
 import com.server.delivery.common.response.ResponseMessage;
@@ -25,49 +9,56 @@ import com.server.delivery.domain.payment.dto.res.PaymentCancelResponseDto;
 import com.server.delivery.domain.payment.dto.res.PaymentConfirmResponseDto;
 import com.server.delivery.domain.payment.dto.res.PaymentSearchResponseDto;
 import com.server.delivery.domain.payment.service.PaymentService;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class PaymentController {
-	private final PaymentService paymentService;
+    private final PaymentService paymentService;
 
-	// 결제 승인
-	@ResponseStatus(HttpStatus.CREATED)
-	@PostMapping("/v1/payments/confirm")
-	public CustomResponse<PaymentConfirmResponseDto> confirmPayment(
-		@AuthenticationPrincipal CustomUserDetail userDetail,
-		@RequestBody PaymentConfirmRequestDto request) {
-		PaymentConfirmResponseDto response = paymentService.confirmPayment(userDetail, request);
-		return CustomResponse.success(ResponseMessage.PAYMENT_REQUEST.getMessage(), response);
-	}
+    // 결제 승인
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/v1/payments/confirm")
+    public CustomResponse<PaymentConfirmResponseDto> confirmPayment(
+            @AuthenticationPrincipal CustomUserDetail userDetail,
+            @RequestBody PaymentConfirmRequestDto request) {
+        PaymentConfirmResponseDto response = paymentService.confirmPayment(userDetail, request);
+        return CustomResponse.success(ResponseMessage.PAYMENT_REQUEST.getMessage(), response);
+    }
 
-	// 결제 취소
-	@ResponseStatus(HttpStatus.CREATED)
-	@PostMapping("/v1/payments/{paymentId}/cancel")
-	public CustomResponse<PaymentCancelResponseDto> cancelPayment(
-		@PathVariable UUID paymentId,
-		@RequestBody PaymentCancelRequestDto request
-	) {
-		PaymentCancelResponseDto response = paymentService.cancelPayment(paymentId, request);
-		return CustomResponse.success(ResponseMessage.PAYMENT_CANCEL.getMessage(), response);
-	}
+    // 결제 취소
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/v1/payments/{paymentId}/cancel")
+    public CustomResponse<PaymentCancelResponseDto> cancelPayment(
+            @PathVariable UUID paymentId,
+            @RequestBody PaymentCancelRequestDto request
+    ) {
+        PaymentCancelResponseDto response = paymentService.cancelPayment(paymentId, request);
+        return CustomResponse.success(ResponseMessage.PAYMENT_CANCEL.getMessage(), response);
+    }
 
-	@ResponseStatus(HttpStatus.OK)
-	@GetMapping("/v1/payments/{paymentId}")
-	public CustomResponse<PaymentSearchResponseDto> searchPayment(@PathVariable UUID paymentId) {
-		PaymentSearchResponseDto response = paymentService.searchPayment(paymentId);
-		return CustomResponse.success(ResponseMessage.PAYMENT_SEARCH.getMessage(), response);
-	}
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/v1/payments/{paymentId}")
+    public CustomResponse<PaymentSearchResponseDto> searchPayment(@PathVariable UUID paymentId) {
+        PaymentSearchResponseDto response = paymentService.searchPayment(paymentId);
+        return CustomResponse.success(ResponseMessage.PAYMENT_SEARCH.getMessage(), response);
+    }
 
-	@ResponseStatus(HttpStatus.OK)
-	@GetMapping("/v1/payments")
-	public CustomResponse<PagedModel<PaymentSearchResponseDto>> searchPayment(
-		@AuthenticationPrincipal CustomUserDetail userDetail,
-		@PageableDefault(direction = Sort.Direction.DESC, size = 10) Pageable pageable) {
-		PagedModel<PaymentSearchResponseDto> response = paymentService.searchPayments(userDetail.getUserId(), pageable);
-		return CustomResponse.success(ResponseMessage.PAYMENT_SEARCH_ALL.getMessage(), response);
-	}
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/v1/payments")
+    public CustomResponse<PagedModel<PaymentSearchResponseDto>> searchPayment(
+            @AuthenticationPrincipal CustomUserDetail userDetail,
+            @PageableDefault(page = 0, size = 10) Pageable pageable) {
+        PagedModel<PaymentSearchResponseDto> response = paymentService.searchPayments(userDetail.getUserId(), pageable);
+        return CustomResponse.success(ResponseMessage.PAYMENT_SEARCH_ALL.getMessage(), response);
+    }
 }
--- src/main/java/com/server/delivery/domain/payment/repository/PaymentJpaRepository.java
+++ src/main/java/com/server/delivery/domain/payment/repository/PaymentJpaRepository.java
@@ -1,25 +1,20 @@
 package com.server.delivery.domain.payment.repository;
 
-import java.util.Optional;
-import java.util.UUID;
-
+import com.server.delivery.model.payment.Payment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.server.delivery.model.payment.Payment;
+import java.util.UUID;
 
 public interface PaymentJpaRepository extends JpaRepository<Payment, UUID> {
-	@Query("select exists (select 1 from Payment p where p.order.orderUuid = :serverOrderId)")
-	boolean existsByOrderUuid(UUID serverOrderId);
+    @Query("select exists (select 1 from Payment p where p.order.orderUuid = :serverOrderId)")
+    boolean existsByOrderUuid(UUID serverOrderId);
 
-	@Query("select p from Payment p "
-		+ "join fetch p.user "
-		+ "where p.user.id = :userId "
-		+ "order by p.order.createdAt desc")
-	Page<Payment> searchUserReviews(Long userId, Pageable pageable);
-
-	@Query("select p from Payment p where p.paymentUuid = :paymentUuid")
-	Optional<Payment> findByPaymentUuid(UUID paymentUuid);
+    @Query("select p from Payment p "
+            + "join fetch p.user "
+            + "where p.user.id = :userId "
+            + "order by p.paidAt desc, p.cancelDetails.canceledAt desc")
+    Page<Payment> searchUserReviews(Long userId, Pageable pageable);
 }
