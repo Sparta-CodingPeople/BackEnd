da3c42cdbab882dfc967b7cf09ad820c66e8847d
kizzis <iswater.kim@gmail.com>
2025-02-24 19:52:45 +0900

    [Refactor] 배달 조회 API, 배달 목록 조회 API 추가

--- src/main/java/com/server/delivery/domain/delivery/repository/DeliveryJpaRepository.java
+++ src/main/java/com/server/delivery/domain/delivery/repository/DeliveryJpaRepository.java
@@ -1,10 +1,19 @@
 package com.server.delivery.domain.delivery.repository;
 
+import com.server.delivery.model.delivery.entity.Delivery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.server.delivery.model.delivery.entity.Delivery;
-
 public interface DeliveryJpaRepository extends JpaRepository<Delivery, UUID> {
+    @Query("select d from Delivery d "
+            + "join fetch d.order o "
+            + "join fetch o.orderMenus om "
+            + "where (:keyword is null "
+            + "or o.deliveryAddress like %:keyword% "
+            + "or om.menu.menuName like %:keyword%)")
+    Page<Delivery> searchDeliveries(String keyword, Pageable validatedPageable);
 }
