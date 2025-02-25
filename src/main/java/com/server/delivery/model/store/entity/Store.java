371db70b8ab48632427a9b5a8057b30b1c855042
Sunro1994 <gwangjulsr@gmail.com>
2025-02-25 14:38:35 +0900

    [Refactor] UserStore 엔티티,레포지터리 삭제

--- src/main/java/com/server/delivery/model/store/entity/Store.java
+++ src/main/java/com/server/delivery/model/store/entity/Store.java
@@ -4,15 +4,13 @@ import com.server.delivery.common.BaseEntity;
 import com.server.delivery.model.manager.entity.Manager;
 import com.server.delivery.model.menu.entity.Menu;
 import com.server.delivery.model.order.entity.Order;
-import com.server.delivery.model.owner.entity.OwnerStore;
+import com.server.delivery.model.owner.entity.Owner;
 import com.server.delivery.model.review.entity.Review;
-import com.server.delivery.model.user.entity.UserStore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -50,9 +48,6 @@ public class Store extends BaseEntity {
     private boolean storeIsGranted = Boolean.FALSE;
 
     @OneToMany(mappedBy = "store")
-    private List<UserStore> userStore;
-
-    @OneToMany(mappedBy = "store")
     private List<Menu> menus;
 
     @OneToMany(mappedBy = "store")
@@ -61,20 +56,20 @@ public class Store extends BaseEntity {
     @OneToMany(mappedBy = "store")
     private List<StoreOperationTimes> operatingHours;
 
-    @Builder.Default
     @OneToMany(mappedBy = "store")
-    private List<Review> reviews = new ArrayList<>();
+    private List<Review> reviews;
 
     //StoreCategoryMapping을 통해 매장과 카테고리를 연결
     @OneToMany(mappedBy = "store")
-    private List<StoreCategoryMapping> categoryMappings = new ArrayList<>();
+    private List<StoreCategoryMapping> categoryMappings;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "location_uuid")
     private Location location;
 
-    @OneToMany(mappedBy = "store")
-    private List<OwnerStore> ownerStore;
+    @ManyToOne(fetch = FetchType.LAZY)  // 하나의 매장은 한 명의 점주만 가질 수 있음
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
 
     @OneToOne(mappedBy = "store")  // 매장에 매니저 한 명만 가능
     private Manager manager;  // 추가된 부분
--- src/main/java/com/server/delivery/model/store/repository/userStore/UserStoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.server.delivery.model.store.repository.userStore;
-
-import com.server.delivery.model.user.entity.UserStore;
-
-public interface UserStoreRepository {
-    UserStore save(UserStore userStore);
-}
